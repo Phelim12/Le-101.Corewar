cf0c81a6bde33d8a33b282ef6fad001930b67433
Dorian Guelpa <dguelpa@z3r2p4.le-101.fr>
2018-06-01 15:38:14 +0200

    yo

--- Rendu/VM_Corewar/Includes/main_vm.h
+++ Rendu/VM_Corewar/Includes/main_vm.h
@@ -6,23 +6,37 @@
 /*   By: clcreuso <clcreuso@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/05/29 16:14:53 by clcreuso     #+#   ##    ##    #+#       */
-/*   Updated: 2018/05/29 16:14:53 by clcreuso    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/06/01 15:09:41 by dguelpa     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef MAIN_VM
 # define MAIN_VM
+# define USE "Usage: ./corewar [-d N] [[-n number] \
+champion1.cor] ...\n\t-d N\t: dump memory after nbr_cycles \
+then exit\n\t-n number\t: set the number of the next player\n"
 
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../../Libft/Includes/libft.h"
+#include "../../Libft/Includes/ft_printf.h"
+
+typedef struct	s_vm
+{
+	int		dump;
+	int		d_cycles;
+	int		nb_players;
+	int		*num_champs;
+	char	**champions;
+}				t_vm;
 
 /*
 **┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-**┃ Functions alloweds:                                                        ┃
+**┃ Functions allowed:                                                         ┃
 **┃  ◦ open                                                                    ┃
 **┃  ◦ read                                                                    ┃
 **┃  ◦ write                                                                   ┃
--- Rendu/VM_Corewar/Sources/main_vm.c
+++ Rendu/VM_Corewar/Sources/main_vm.c
@@ -6,16 +6,124 @@
 /*   By: clcreuso <clcreuso@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/05/29 16:14:38 by clcreuso     #+#   ##    ##    #+#       */
-/*   Updated: 2018/05/29 16:14:38 by clcreuso    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/06/01 15:37:58 by dguelpa     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "main_vm.h"
 
-int main(int argc, char const *argv[])
+t_vm	*g_vm;
+
+void	print_usage(void)
 {
-	if (argc == 2)
-		printf("%s\n", argv[1]);
+	ft_printf(USE);
+}
+
+void	ft_error(char *s)
+{
+	int i;
+
+	i = 0;
+	while (i < g_vm->nb_players)
+		free(g_vm->champions[i++]);
+	free(g_vm->champions);
+	free(g_vm->num_champs);
+	free(g_vm);
+	ft_printf(s);
+	exit(1);
+}
+
+void	parse_args(char const **argv)
+{
+	int	i;
+	int	num;
+	int	k;
+
+	i = 1;
+	k = 0;
+	num = 0;
+	while (argv[i])
+	{
+		if (!ft_strcmp("-dump", argv[i]))
+		{
+			g_vm->dump = 1;
+			if (argv[i + 1] && ft_str_is_number(argv[i + 1]) &&
+					ft_atoi(argv[i + 1]) >= 0)
+				g_vm->d_cycles = ft_atoi(argv[i]);
+			else
+				ft_error("Wrong parameter for -dump option\n");
+			i += 2;
+		}
+		else if (!ft_strcmp("-n", argv[i]))
+		{
+			//apres -n, on peut considerer que les deux prochains arguments doivent
+			//etre un numero de joueur disponible puis un champion, pas d'autres options genre pas -n 1 -dump 54 test.cor
+			if (argv[i + 1] && ft_str_is_number(argv[i + 1]) &&
+			ft_atoi(argv[i + 1]) >= 0)
+				g_vm->num_champs[k] = ft_atoi(argv[i + 1]);
+			else
+				ft_error("Wrong parameter for -n option\n");
+			if (argv[i + 2] &&
+			!ft_strcmp(".cor", &argv[i + 2][ft_strlen(argv[i + 2]) - 4]))
+				g_vm->champions[k] = ft_strdup(argv[i + 2]);
+			else
+				ft_error("After -n, no file detected, or wrong extension\n");
+			k++;
+			i += 3;
+		}
+		else if (!ft_strcmp(".cor", &argv[i][ft_strlen(argv[i]) - 4]))
+		{
+			g_vm->num_champs[k] = num++;
+			g_vm->champions[k++] = ft_strdup(argv[i++]);
+		}
+		else
+			ft_error("An error occured in the argument passed to corewar\n");
+	}
+}
+
+void	init_champs(char const **argv)
+{
+	void	*tmp;
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (!ft_strcmp(".cor", &argv[i][ft_strlen(argv[i]) - 4]))
+			g_vm->nb_players++;
+		i++;
+	}
+	if ((tmp = (int*)malloc(sizeof(int) * g_vm->nb_players + 1)))
+		g_vm->num_champs = tmp;
+	if ((tmp = (char**)malloc(sizeof(char*) * g_vm->nb_players + 1)))
+		g_vm->champions = tmp;
+	i = 0;
+	while (i < g_vm->nb_players)
+		g_vm->champions[i++] = NULL;
+	g_vm->champions[i] = NULL;
+}
+
+void	init_vm(char const **argv)
+{
+	void	*tmp;
+
+	if ((tmp = (t_vm*)malloc(sizeof(t_vm))))
+		g_vm = tmp;
+	g_vm->dump = 0;
+	g_vm->d_cycles = 0;
+	g_vm->nb_players = 0;
+	init_champs(argv);
+}
+
+int		main(int argc, char const **argv)
+{
+	if (argc > 1)
+	{
+		init_vm(argv);
+		parse_args(argv);
+	}
+	else
+		print_usage();
 	return (0);
 }
