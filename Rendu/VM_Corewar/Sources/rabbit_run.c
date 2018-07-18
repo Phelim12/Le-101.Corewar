1b0a3de532c27a549a91c83ae11b74076e28df1e
Julien Janin-reynaud <jjanin-r@z1r7p6.le-101.fr>
2018-07-18 19:07:35 +0200

    exec process

--- Rendu/VM_Corewar/Sources/rabbit_run.c
+++ Rendu/VM_Corewar/Sources/rabbit_run.c
@@ -6,7 +6,7 @@
 /*   By: dguelpa <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/22 14:46:51 by dguelpa      #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/18 18:53:12 by jjanin-r    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/18 19:07:20 by jjanin-r    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -62,7 +62,7 @@ int		cycling(void)
 	{
 		cycles_passed = check_destruction_process(cycles_passed);
 		cycle_process();//remplissage de la fetchqueue ou delai, ou exec d'autre chose qu'un fork ou un live ou une ecriture memoire
-		exec_process();
+//		exec_process();
 		//read_memory(); //stocker les infos en memoire. pourquoi pas carrement dupliquer g_vm avant et apres changement ? dans cycle_process ?
 		//write_memory(); //si ecriture simultanee, parralelisme : le dernier process cree joue en premier.
 			// /!\ /!\
