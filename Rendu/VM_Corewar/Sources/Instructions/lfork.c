af20c20ab1cb37a2079b9eb01bfa0034d99d3d21
Nassim Bettach <nbettach@z1r10p4.le-101.fr>
2018-07-24 15:55:26 +0200

    999 OK

--- Rendu/VM_Corewar/Sources/Instructions/lfork.c
+++ Rendu/VM_Corewar/Sources/Instructions/lfork.c
@@ -6,7 +6,7 @@
 /*   By: nbettach <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/22 14:54:13 by nbettach     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/24 14:28:11 by nbettach    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/24 15:43:09 by nbettach    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,12 +20,17 @@ void		debug_lfork(int aim)
 
 void		ft_lfork(t_process **proc, t_process **begin)
 {
-	int				aim;
 	t_process		*new;
+	int				aim;
 
-	aim = (*proc)->begin + (*proc)->fetchqueue[0][1];
-//	debug_lfork(aim);
-	new = lstnew_vm((*proc)->registers, REG_SIZE * REG_NUMBER);
+	new = lstnew_vm((*proc)->registers, REG_SIZE * (REG_NUMBER + 1));
+	aim = ((*proc)->begin + ((*proc)->fetchqueue[0][1] % IDX_MOD) % MEM_SIZE);
+	new->begin = aim;
+	new->cycle_delay = -1;
+	new->carry = (*proc)->carry;
 	new->registers[0] = aim;
-	lstadd_vm(begin, new);
+	g_vm->nb_proc++;
+	new->num = g_vm->nb_proc;
+	new->next = *begin;
+	*begin = new;
 }
