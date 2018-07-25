ed4df2a52bb1c4e0e75082ba6a00f7dfed5a1ae0
Clement Creusot <clcreuso@z1r11p3.le-101.fr>
2018-07-25 22:28:32 +0200

    COREWAR MSG:pull

--- Rendu/ASM_Corewar/Sources/Error/error_token.c
+++ Rendu/ASM_Corewar/Sources/Error/error_token.c
@@ -72,3 +72,41 @@ void	print_error_token(t_line *file, t_cmd *cmd, int msg_error)
 	free_file(file);
 	exit(EXIT_FAILURE);
 }
+
+/*
+P1 | live -1						P1 |	sti	r13, r16, r12		PC -> 3073		Cycle 1806
+P6 | zjmp 502 OK					P2 |	live	-1		PC -> 42		Cycle 1806
+P3 | sti r13 509 0					P7 |	zjmp	502		PC -> 2571		Cycle 1807
+P1 | st r16 -300					P4 |	sti	r13, r16, r12		PC -> 3073		Cycle 1807
+P8 | sti r3 28 0					P2 |	st	r16, -300		PC -> 47		Cycle 1811
+P7 | sti r3 495 0					P8 |	sti	r3, r4, r5		PC -> 1675		Cycle 1812
+P1 | st r2 -301						P6 |	sti	r3, r4, r5		PC -> 1675		Cycle 1812
+P1 | st r2 -302						P2 |	st	r2, -301		PC -> 52		Cycle 1816
+P4 | zjmp 19 OK						P2 |	st	r2, -302		PC -> 57		Cycle 1821
+P2 | zjmp 500 OK					P3 |	zjmp	19		PC -> 1680		Cycle 1822
+P3 | zjmp 500 OK					P1 |	zjmp	500		PC -> 3078		Cycle 1826
+P5 | sti r13 511 0					P4 |	zjmp	500		PC -> 3078		Cycle 1827
+P1 | sub r11 r12 r11				P5 |	sti	r13, r16, r12		PC -> 3073		Cycle 1831
+P8 | zjmp 19 OK						P2 |	sub	r11, r12, r11		PC -> 62		Cycle 1831
+P7 | zjmp 19 OK						P8 |	zjmp	19		PC -> 1680		Cycle 1832
+P6 | sti r13 507 0					P7 |	sti	r13, r16, r12		PC -> 3073		Cycle 1832
+P2 | xor 190056971 0 r9				P6 |	zjmp	19		PC -> 1680		Cycle 1832
+P3 | xor 201916918 0 r9				P1 |	xor	r9, r12, r9		PC -> 3578		Cycle 1832
+P4 | sti r3 24 0					P4 |	xor	r9, r12, r9		PC -> 3578		Cycle 1833
+P5 | zjmp 500 OK					P3 |	sti	r3, r4, r5		PC -> 1699		Cycle 1847
+P1 | zjmp 33 FAILED					P5 |	zjmp	500		PC -> 3078		Cycle 1851
+P6 | zjmp 500 OK					P2 |	zjmp	33		PC -> 67		Cycle 1851
+P1 | ld 0 r16						P7 |	zjmp	500		PC -> 3078		Cycle 1852
+P8 | sti r3 28 0					P2 |	ld	%0, r16		PC -> 70		Cycle 1856
+P7 | sti r3 495 0					P8 |	sti	r3, r4, r5		PC -> 1699		Cycle 1857
+P5 | xor 190056971 0 r9				P6 |	sti	r3, r4, r5		PC -> 1699		Cycle 1857
+P2 | sti r2 511 0					P5 |	xor	r9, r12, r9		PC -> 3578		Cycle 1857
+P6 | xor 201916918 0 r9				P1 |	sti	r2, r15, r12		PC -> 3583		Cycle 1857
+P3 | sti r2 2 0						P7 |	xor	r9, r12, r9		PC -> 3578		Cycle 1858
+P4 | zjmp 19 OK						P4 |	sti	r2, r15, r12		PC -> 3583		Cycle 1858
+P1 | zjmp -35 OK					P3 |	zjmp	19		PC -> 1704		Cycle 1867
+P8 | zjmp 19 OK						P2 |	zjmp	-35		PC -> 77		Cycle 1876
+P7 | zjmp 19 OK						P8 |	zjmp	19		PC -> 1704		Cycle 1877
+P5 | sti r2 6 0						P6 |	zjmp	19		PC -> 1704		Cycle 1877
+P2 | sti r5 511 0					P5 |	sti	r2, r15, r12		PC -> 3583		Cycle 1882
+*/
--- Rendu/TEST_VM/test_vm.sh
+++ Rendu/TEST_VM/test_vm.sh
@@ -46,9 +46,9 @@ do
 	for P2 in $FILES
 	do
 
-#		if [ $NAME1 != "Douceur_power" ]; then
-#			break 
-#		fi
+		if [ $NAME1 != "Explosive_Kitty" ]; then
+			break 
+		fi
 
 		NAME2=$(echo $P2 | rev | cut -c5- | rev)
 
