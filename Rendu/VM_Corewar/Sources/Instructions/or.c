68dbe16d4b9e3a6c08f127557641030c0f7c5f3d
Nassim Bettach <nbettach@z1r10p4.le-101.fr>
2018-07-26 02:51:13 +0200

    update ADD / OR / XOR

--- Rendu/VM_Corewar/Sources/Instructions/or.c
+++ Rendu/VM_Corewar/Sources/Instructions/or.c
@@ -6,7 +6,7 @@
 /*   By: nbettach <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/22 15:12:23 by nbettach     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/22 15:17:18 by nbettach    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/26 02:27:59 by nbettach    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -28,7 +28,7 @@ void		or_reg(t_process **proc)
 	else
 	{
 		(*proc)->registers[(*proc)->fetchqueue[2][1]] =
-			read_map(((*proc)->begin + (*proc)->fetchqueue[0][1]) % IDX_MOD, 4)
+			read_map((*proc)->begin + (*proc)->fetchqueue[0][1] % IDX_MOD, 4)
 			| (*proc)->fetchqueue[1][1];
 	}
 }
@@ -50,7 +50,7 @@ void		or_dir(t_process **proc)
 	else
 	{
 		(*proc)->registers[(*proc)->fetchqueue[2][1]] =
-			read_map(((*proc)->begin + (*proc)->fetchqueue[1][1]) % IDX_MOD, 4)
+			read_map((*proc)->begin + (*proc)->fetchqueue[1][1] % IDX_MOD, 4)
 			| (*proc)->fetchqueue[0][1];
 	}
 }
@@ -60,19 +60,19 @@ void		or_ind(t_process **proc)
 	if ((*proc)->fetchqueue[1][0] == 1)
 	{
 		(*proc)->registers[(*proc)->fetchqueue[2][1]] =
-			read_map(((*proc)->begin + (*proc)->fetchqueue[0][1]) % IDX_MOD, 4)
+			read_map((*proc)->begin + (*proc)->fetchqueue[0][1] % IDX_MOD, 4)
 			| (*proc)->registers[(*proc)->fetchqueue[1][1]];
 	}
 	else if ((*proc)->fetchqueue[1][0] == 2)
 	{
 		(*proc)->registers[(*proc)->fetchqueue[2][1]] =
-			read_map(((*proc)->begin + (*proc)->fetchqueue[0][1]) % IDX_MOD, 4)
+			read_map((*proc)->begin + (*proc)->fetchqueue[0][1] % IDX_MOD, 4)
 			| (*proc)->fetchqueue[1][1];
 	}
 	else
 	{
 		(*proc)->registers[(*proc)->fetchqueue[2][1]] =
-			read_map(((*proc)->begin + (*proc)->fetchqueue[1][1]) % IDX_MOD, 4)
+			read_map((*proc)->begin + (*proc)->fetchqueue[1][1] % IDX_MOD, 4)
 			| read_map(((*proc)->begin + (*proc)->fetchqueue[0][1])
 					% IDX_MOD, 4);
 	}
