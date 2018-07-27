68017f75b6b4a10e4dff02d3293dd08133e9214c
Julien Janin-reynaud <jjanin-r@z3r5p2.le-101.fr>
2018-07-27 18:33:41 +0200

    update exec.c

--- Rendu/VM_Corewar/Sources/exec.c
+++ Rendu/VM_Corewar/Sources/exec.c
@@ -6,7 +6,7 @@
 /*   By: jjanin-r <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/27 04:12:03 by jjanin-r     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/27 04:12:05 by jjanin-r    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/27 18:33:20 by jjanin-r    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,19 +17,19 @@ void			exec_2(t_process **proc, t_process **begin)
 {
 	if (PROC->op == ZJMP)
 		ft_zjmp(proc);
-	if (PROC->op == LDI)
+	else if (PROC->op == LDI)
 		ft_ldi(proc);
-	if (PROC->op == STI)
+	else if (PROC->op == STI)
 		ft_sti(proc);
-	if (PROC->op == FORK)
+	else if (PROC->op == FORK)
 		ft_fork(proc, begin);
-	if (PROC->op == LLD)
+	else if (PROC->op == LLD)
 		ft_lld(proc);
-	if (PROC->op == LLDI)
+	else if (PROC->op == LLDI)
 		ft_lldi(proc);
-	if (PROC->op == LFORK)
+	else if (PROC->op == LFORK)
 		ft_lfork(proc, begin);
-	if (PROC->op == AFF)
+	else if (PROC->op == AFF)
 		ft_aff(proc);
 }
 
@@ -37,19 +37,19 @@ static void		exec_1(t_process **proc)
 {
 	if (PROC->op == LIVE)
 		ft_live(proc);
-	if (PROC->op == LD)
+	else if (PROC->op == LD)
 		ft_ld(proc);
-	if (PROC->op == ST)
+	else if (PROC->op == ST)
 		ft_st(proc);
-	if (PROC->op == ADD)
+	else if (PROC->op == ADD)
 		ft_add(proc);
-	if (PROC->op == SUB)
+	else if (PROC->op == SUB)
 		ft_sub(proc);
-	if (PROC->op == AND)
+	else if (PROC->op == AND)
 		ft_and(proc);
-	if (PROC->op == OR)
+	else if (PROC->op == OR)
 		ft_or(proc);
-	if (PROC->op == XOR)
+	else if (PROC->op == XOR)
 		ft_xor(proc);
 }
 
@@ -57,6 +57,6 @@ void			exec(t_process **proc, t_process **begin)
 {
 	if (PROC->op >= LIVE && PROC->op <= XOR)
 		exec_1(proc);
-	if (PROC->op >= ZJMP && PROC->op <= AFF)
+	else if (PROC->op >= ZJMP && PROC->op <= AFF)
 		exec_2(proc, begin);
 }
