2001eade3e3b91c8f2bc633859f82ef134bfc74e
Julien Janin-reynaud <jjanin-r@z1r11p1.le-101.fr>
2018-07-27 02:47:10 +0200

    added init_champs.c && norme

--- Rendu/VM_Corewar/Sources/init.c
+++ Rendu/VM_Corewar/Sources/init.c
@@ -6,7 +6,7 @@
 /*   By: dguelpa <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/05 17:33:45 by dguelpa      #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/27 01:27:07 by jjanin-r    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/27 02:38:25 by jjanin-r    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -38,93 +38,6 @@ int			init_process(unsigned int a)
 		free(tmp_reg);
 	}
 	return (0);
-}
-
-static void	sort_champ_tab(void)
-{
-	t_champ			*tmp;
-	unsigned int	i;
-
-	i = -1;
-	while (++i < g_vm->nb_players - 1)
-	{
-		if (g_vm->champion[i]->num < g_vm->champion[i + 1]->num)
-		{
-			tmp = g_vm->champion[i];
-			g_vm->champion[i] = g_vm->champion[i + 1];
-			g_vm->champion[i + 1] = tmp;
-			i = -1;
-		}
-	}
-}
-
-static int	sub2_init_champ(void)
-{
-	int	a;
-
-	a = g_vm->nb_players;
-	sort_champ_tab();
-	while (--a >= 0)
-		if (init_process(a) == -1 || get_champ(a) == -1)
-			return (-1);
-	return (0);
-}
-
-static void	sub_init_champ(void)
-{
-	unsigned int	i;
-	long			num;
-	unsigned int	j;
-
-	i = -1;
-	num = 1;
-	while (++i < g_vm->nb_players)
-	{
-		num = 1;
-		g_vm->champion[i]->live = 0;
-		if (g_vm->champion[i]->num == 0)
-		{
-			j = -1;
-			while (++j < g_vm->nb_players)
-			{
-				if (g_vm->champion[j]->num == num)
-				{
-					num++;
-					j = -1;
-				}
-			}
-			g_vm->champion[i]->num = num;
-		}
-	}
-
-	sub2_init_champ();
-}
-
-void		init_champs(char const **argv)
-{
-	void			*tmp;
-	unsigned int	i;
-
-	i = 0;
-	while (argv[i])
-	{
-		if (!ft_strcmp(".cor", &argv[i][ft_strlen(argv[i]) - 4]))
-			g_vm->nb_players++;
-		i++;
-	}
-	i = 0;
-	if ((tmp = (t_champ**)malloc(sizeof(t_champ*) * g_vm->nb_players + 1)))
-		g_vm->champion = tmp;
-	while (i < g_vm->nb_players)
-	{
-		if ((tmp = (t_champ*)malloc(sizeof(t_champ))))
-			g_vm->champion[i] = tmp;
-		g_vm->champion[i++]->num = 0;
-	}
-	if (g_vm->nb_players > MAX_PLAYERS)
-		error_vm("Too many .cor files in parameters\n", 0);
-	parse_args(argv);
-	sub_init_champ();
 }
 
 int			init_map(void)
---
+++ Rendu/VM_Corewar/Sources/init_champs.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   init_champs.c                                    .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jjanin-r <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/07/27 02:36:04 by jjanin-r     #+#   ##    ##    #+#       */
+/*   Updated: 2018/07/27 02:45:05 by jjanin-r    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../Includes/main_vm.h"
+
+static void	sort_champ_tab(void)
+{
+	t_champ			*tmp;
+	unsigned int	i;
+	int	a;
+
+	a = g_vm->nb_players;
+	i = -1;
+	while (++i < g_vm->nb_players - 1)
+	{
+		if (g_vm->champion[i]->num < g_vm->champion[i + 1]->num)
+		{
+			tmp = g_vm->champion[i];
+			g_vm->champion[i] = g_vm->champion[i + 1];
+			g_vm->champion[i + 1] = tmp;
+			i = -1;
+		}
+	}
+	while (--a >= 0)
+		if (init_process(a) == -1 || get_champ(a) == -1)
+			error_vm("malloc failed\n", 0);
+}
+
+static void		init_champ_num(void)
+{
+	unsigned int	i;
+	long			num;
+	unsigned int	j;
+
+	i = -1;
+	num = 1;
+	while (++i < g_vm->nb_players)
+	{
+		num = 1;
+		g_vm->champion[i]->live = 0;
+		if (g_vm->champion[i]->num == 0)
+		{
+			j = -1;
+			while (++j < g_vm->nb_players)
+			{
+				if (g_vm->champion[j]->num == num)
+				{
+					num++;
+					j = -1;
+				}
+			}
+			g_vm->champion[i]->num = num;
+		}
+	}
+	sort_champ_tab();
+}
+
+void		init_champs(char const **argv)
+{
+	void			*tmp;
+	unsigned int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (!ft_strcmp(".cor", &argv[i][ft_strlen(argv[i]) - 4]))
+			g_vm->nb_players++;
+		i++;
+	}
+	i = 0;
+	if ((tmp = (t_champ**)malloc(sizeof(t_champ*) * g_vm->nb_players + 1)))
+		g_vm->champion = tmp;
+	while (i < g_vm->nb_players)
+	{
+		if ((tmp = (t_champ*)malloc(sizeof(t_champ))))
+			g_vm->champion[i] = tmp;
+		g_vm->champion[i++]->num = 0;
+	}
+	if (g_vm->nb_players > MAX_PLAYERS)
+		error_vm("Too many .cor files in parameters\n", 0);
+	parse_args(argv);
+	init_champ_num();
+}
